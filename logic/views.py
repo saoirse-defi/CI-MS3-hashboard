ad704ed763560cf65a92eee97f394e92169a485f
saoirse-defi <saoirsef@yahoo.co.uk>
2021-06-23 12:11:23 +0000

    moving files within dev env to solve initial deployment issues

--- logic/views.py
+++ logic/views.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, url_for, Blueprint, render_template
 from flask_login import LoginManager, login_required
 from run import app
-from . import models
+from logic.models import User
 from datetime import datetime, date
 import time
 from dateutil import parser
@@ -9,23 +9,17 @@ from dateutil import parser
 views = Blueprint('views', __name__)
 
 
-@views.route("/")
-def index():
-    return render_template("index.html")
-
-
-@views.route('/signup', methods=['GET', 'POST'])
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return User().signup()
+    return render_template("signup.html")
 
 
-@views.route('/login', methods=['GET', 'POST']) 
+@app.route('/login', methods=['GET', 'POST']) 
 def login():
     return render_template("login.html")
 
 
-@views.route('/logout')
+@app.route('/logout')
 @login_required
 def logout():
-    logout_user()
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('views.login'))
