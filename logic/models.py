18017d3758656681cdd9549ac43ec3abc19523ff
saoirse-defi <saoirsef@yahoo.co.uk>
2021-06-23 16:39:06 +0000

    add config variables, pulling live data from mongo collection

--- logic/models.py
+++ logic/models.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import pymongo
 
 
 class User:
@@ -6,6 +7,7 @@ class User:
     def signup(self):
         user = {
             "_id": "",
+            "name": "",
             "email": "",
             "eth": "",
             "password": "",
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import os
-from flask import Flask, render_template, redirect, url_for
-from logic import views
+from flask import Flask, render_template, flash, redirect, request, session, url_for
+from flask_login import LoginManager, login_required
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from logic.models import User
 
 if os.path.exists("env.py"):
     import env
@@ -8,14 +11,39 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECREY_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/get_user")
+def get_user():
+    users = mongo.db.Users.find()
+    return render_template("users.html", users=users)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template("signup.html")
+
+
+@app.route('/login', methods=['GET', 'POST']) 
+def login():
+    return render_template("login.html")
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    return redirect(url_for('views.login'))
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+
 
