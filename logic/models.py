9adcc81def946e75eacfe66315a1d513a90afc0a
saoirse-defi <saoirsef@yahoo.co.uk>
2021-06-25 15:31:31 +0000

    [Login account system functional, users stored in mongodb]

--- logic/models.py
+++ logic/models.py
@@ -1,12 +1,15 @@
-from flask import Flask, jsonify
+import uuid
+from flask import Flask, jsonify, session, redirect, request, url_for
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 import pymongo
+from run import mongo
 
 
-class User:
+class User(UserMixin):
 
     def signup(self):
         user = {
-            "_id": "",
             "name": "",
             "email": "",
             "eth": "",
@@ -15,3 +18,58 @@ class User:
         }
         
         return jsonify(user), 200
+    
+    def is_active(self):
+        return True
+   
+    def start_session(self, user):
+        session['logged_in'] = True
+        session['user'] = user
+        return jsonify(user), 200
+    
+    def signout(self):
+        session.clear()
+        return redirect('/')
+
+
+class Account():
+
+    def signup(self):
+        account = {
+            "_id": uuid.uuid4().hex,
+            "name": request.form.get('name'),
+            "email": request.form.get('email'),
+            "eth": request.form.get('eth'),
+            "password": request.form.get('password')
+        }
+
+        account['password'] = generate_password_hash(account['password'])
+
+        if(mongo.db.User.find_one({'email': account['email']})):
+            return jsonify({'error': 'Email already in use.'})
+
+        if mongo.db.User.insert_one(account):
+            return self.start_session(account)
+
+        return jsonify({'error': 'Signup failed'}), 400
+    
+    def start_session(self, account):
+        del account['password']
+        session['logged_in'] = True
+        session['user'] = account
+
+        return jsonify(account), 200
+
+    def signout(self):
+        session.clear()
+        return redirect(url_for('login'))
+    
+    def login(self):
+        existing_user = mongo.db.User.find_one({
+            "email": request.form.get('email')
+        })
+
+        if existing_user:
+            return self.start_session(existing_user)
+        
+        return jsonify({"error": "Invalid login details"}), 401
--- run.py
+++ run.py
@@ -1,15 +1,19 @@
+# Imports
 import os
+import uuid
 from os import path
 if os.path.exists("env.py"):
     import env
 
 from flask import Flask, render_template, flash, redirect, request, session, url_for
-from flask_login import login_user, logout_user, current_user, login_required
+from flask_login import login_user, logout_user, current_user, LoginManager
 from flask_pymongo import PyMongo
+from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from bson.objectid import ObjectId
-from logic.models import User
+import logic.models
 
+# Application setup
 
 app = Flask(__name__)
 
@@ -19,13 +23,36 @@ app.config["SESSION_TYPE"] = 'filesystem'
 app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
+login_manager = LoginManager()
 
+# Decorator Functions
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('login'))
+
+    return wrap
+
+# Routes
 
 @app.route("/")
-@app.route("/get_user")
-def get_user():
-    users = mongo.db.Users.find()
-    return render_template("users.html", users=users)
+@app.route("/index")
+@login_required
+def index():
+    return render_template("index.html")
+
+
+@app.route('/signup2', methods=['GET', 'POST'])
+def signup2():
+    if request.method == 'POST':
+        logic.models.Account().signup()
+        return redirect(url_for('index'))
+    return render_template("signup2.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -36,20 +63,17 @@ def signup():
         eth = request.form.get('eth')
         password = request.form.get('password')
         _password = request.form.get('password-confirm')
-
         existing_user = mongo.db.Users.find_one({
             "email": email.lower()
         })
         if existing_user:
             flash("Email address is already taken.", category='error')
-            return redirect(url_for("register"))
         elif len(email) < 4:
             flash('Email Address is too short', category='error')
         elif len(name) < 2:
             flash('Your nickname is too short', category='error')
-        elif eth:
-            if len(eth) != 42:
-                flash('The format of your Ethereum public address is incorrect', category='error')
+        elif len(eth) != 42:
+            flash('The format of your Ethereum public address is incorrect', category='error')
         elif password != _password:
             flash('Your passwords dont match', category='error')
         elif len(password) < 8:
@@ -69,11 +93,25 @@ def signup():
     return render_template("signup.html", user=current_user)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        logic.models.Account().login()
+        return redirect(url_for('index'))
+
+    return render_template("login.html", user=current_user)
+
+
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+
+@app.route('/signout')
+def signout():
+    return logic.models.Account().signout()
 
 
 if __name__ == '__main__':
