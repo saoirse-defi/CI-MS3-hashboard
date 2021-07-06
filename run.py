130d28a23f8cb0ce8fb8cb41fdf0a42d5dcd3b4e
saoirse-defi <saoirsef@yahoo.co.uk>
2021-07-06 11:33:31 +0000

    move global functions, combine 3 async into 1 list

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import json
 import httpx
 import time
 from os import path
+from operator import itemgetter
 from web3 import Web3
 if os.path.exists("env.py"):
     import env
@@ -16,6 +17,7 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from bson.objectid import ObjectId
 import logic.models
+import logic.etherscan_api
 
 # Application setup
 
@@ -32,7 +34,6 @@ transaction_table_headings = ['Date created', 'Hash', 'To', 'From', 'Value', 'To
 
 # Decorator Functions
 
-
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
@@ -43,26 +44,29 @@ def login_required(f):
 
     return wrap
 
+# Formatting functions
+
+def shorten(string):  # formatting functions
+    return "0x..." + string[38:]
+    
+def shorten2(string):
+    return "0x..." + string[62:]
+
+def toInt(x):
+    return int(float(x))
+
+def threeDecimals(y):
+    return "%.3f" % y
+
+
 # Routes
 
 @app.route("/")
 @app.route("/index")
 @login_required
 def index():
-    
-    transactions_list = list(mongo.db.Transaction.find({"from": session['user']['eth']})) # list of cursor
+    transactions_list = list(mongo.db.Transaction.find({"from": session['user']['eth']})) # list of cursor query
 
-    def shorten(string):
-        return "0x..." + string[38:]
-    
-    def shorten2(string):
-        return "0x..." + string[62:]
-
-    def toInt(x):
-        return int(float(x))
-
-    def threeDecimals(y):
-        return "%.3f" % y
     return render_template("index.html",
                             transactions_list=transactions_list,
                             transaction_table_headings=transaction_table_headings,
@@ -75,47 +79,53 @@ def index():
 @app.route('/search', methods=['GET', 'POST'])
 async def search():
     errors = {}
-
-    def shorten(string):
-        return "0x..." + string[38:]
-    
-    def shorten2(string):
-        return "0x..." + string[62:]
-
-    def toInt(x):
-        return int(float(x))
-
-    def threeDecimals(y):
-        return "%.3f" % y
+    transaction_list = []
 
     if request.method == 'POST':
         search_eth = str(request.form.get('search-eth')).lower()
         print(search_eth)
 
         async with httpx.AsyncClient() as client:
-            search_result = await client.get(f'https://api.etherscan.io/api?module=account&action=txlist&address={search_eth}&startblock=0&endblock=99999999&sort=asc&apikey=PQWGH496A8A1H3YV5TKWNVCPHJZ3S7ITHA')
+            eth_res, alt_res, nft_res = await asyncio.gather(
+                client.get(f'https://api.etherscan.io/api?module=account&action=txlist&address={search_eth}&startblock=0&endblock=99999999&sort=asc&apikey=PQWGH496A8A1H3YV5TKWNVCPHJZ3S7ITHA'),
+                client.get(f'https://api.etherscan.io/api?module=account&action=tokentx&address={search_eth}&startblock=0&endblock=999999999&sort=asc&apikey=PQWGH496A8A1H3YV5TKWNVCPHJZ3S7ITHA'),
+                client.get(f'https://api.etherscan.io/api?module=account&action=tokennfttx&address={search_eth}&startblock=0&endblock=999999999&sort=asc&apikey=PQWGH496A8A1H3YV5TKWNVCPHJZ3S7ITHA')
+            )
+
+            # search_result = await client.get(f'https://api.etherscan.io/api?module=account&action=txlist&address={search_eth}&startblock=0&endblock=99999999&sort=asc&apikey=PQWGH496A8A1H3YV5TKWNVCPHJZ3S7ITHA')
         
-        search_result_text = search_result.text
-        search_json = json.loads(search_result_text)
-        transactions = search_json['result']
+        eth_result_text = eth_res.text  # process repsonses into python list
+        eth_json = json.loads(eth_result_text)
+        list_eth = eth_json['result']
 
-        print(transactions)
+        alt_result_text = alt_res.text  # process repsonses into python list
+        alt_json = json.loads(alt_result_text)
+        list_alt = alt_json['result']
 
-        transaction_list = []
+        nft_result_text = nft_res.text  # process repsonses into python list
+        nft_json = json.loads(nft_result_text)
+        list_nft = nft_json['result']
 
-        for transaction in transactions:
+        combined_transaction_list = list_eth + list_alt + list_nft  # combining lists
+
+        for transaction in combined_transaction_list:  # formatting data
             data = {
                 'time': time.strftime("%Y-%m-%d %H:%M", time.localtime(int(transaction['timeStamp']))),
                 'hash': transaction['hash'],
                 'from': transaction['from'],
                 'to': transaction['to'],
-                'value': str(Web3.fromWei(float(transaction['value']), 'ether')),  # in Gwei
-                'error': transaction['isError'],
                 'gas_price': str(Web3.fromWei(int(transaction['gasPrice']), 'ether') * int('1000000000')),
                 'gas_used': str(round(Web3.fromWei(int(transaction['gasPrice']) * int(transaction['gasUsed']), 'ether'), 6)),
-                'starred': False
+                'token_name': transaction['tokenName'] or 'Ethereum',  # not working
+                'token_symbol': transaction['tokenSymbol'] or 'ETH',
+                'contract_address': transaction['contractAddress'],
+                'token_id': int(transaction['tokenID'] or "")
             }
             transaction_list.append(data)
+        
+        transaction_list.sort(reverse=True, key=itemgetter('time'))  # sort combined list by time/date
+
+        print(transaction_list)
 
     return render_template("search.html", 
                             errors=errors, 
@@ -123,8 +133,15 @@ async def search():
                             shorten2=shorten2,
                             toInt=toInt,
                             threeDecimals=threeDecimals,
-                            transaction_list=transaction_list,
+                            transaction_list=transaction_list,  # get this error only sometimes UnboundLocalError: local variable 'transaction_list' referenced before assignment Traceback (most recent call last)
                             transaction_table_headings=transaction_table_headings)
+
+
+@app.route('/_save_transaction')  # background process in order to save transaction to Account fav list
+def _save_transaction(data):
+    logic.models.Account().fav(session['eth'], data)
+    return redirect(url_for('search'))
+
 
 @app.route('/signup2', methods=['GET', 'POST'])
 def signup2():
