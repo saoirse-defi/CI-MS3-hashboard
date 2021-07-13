87b9f81de3a6887356457c31245804b8dd86a95b
saoirse-defi <saoirsef@yahoo.co.uk>
2021-07-13 12:50:01 +0000

    add mobile side menu

--- static/app.js
+++ static/app.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {
+        draggable: true
+    });
   });
 
   
