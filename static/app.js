8046f002eeddc025d02e07161bb291d23453a40d
saoirse-defi <saoirsef@yahoo.co.uk>
2021-07-16 15:08:50 +0000

    remove bootstrap datatable, tooltip

--- static/app.js
+++ static/app.js
@@ -5,28 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-  
-$(document).ready(function(){
-    $('#ethTable').DataTable({
-        "columns": [
-            {"data": "date"},
-            {"data": "hash"},
-            {"data": "to"},
-            {"data": "from"},
-            {"data": "value"},
-            {"data": "token"},
-            {"data": "gasPrice"},
-            {"data": "gasSpent"}
-        ],
-        "pagingType": "simple"
-    });
-    $('#erc20-table').DataTable({
-
-    });
-    $('.dataTables_length').addClass('bs-select');
-    $('td').tooltip(); //bootstrap tooltip 
-})
-
 $(document).ready(() => {
     $('.modal').modal();
 })
