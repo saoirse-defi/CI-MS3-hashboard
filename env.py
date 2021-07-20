97bacd6c85b9a864eb3b73a9006fe9d4b45c436d
saoirse-defi <saoirsef@yahoo.co.uk>
2021-07-20 09:07:10 +0000

    fix pylint error, line to long

--- env.py
+++ env.py
@@ -2,6 +2,8 @@ import os
 
 os.environ.setdefault('IP', "0.0.0.0")
 os.environ.setdefault('MONGO_DBNAME', "hashboard_mongo_db")
-os.environ.setdefault('MONGO_URI', "mongodb+srv://root:root_hashboard_admin@hashboard.sqfij.mongodb.net/hashboard_mongo_db?retryWrites=true&w=majority")
-#  os.environ.setdefault('PORT', "5000")
+os.environ.setdefault('MONGO_URI',
+                      "mongodb+srv://root:root_hashboard_admin@hashboard"
+                      ".sqfij.mongodb.net/hashboard_mongo_db?retryWrites"
+                      "=true&w=majority")
 os.environ.setdefault('SECRET_KEY', "E1]ZJ-M_DMN%5_b99OY&g7UNW!Qt|&")
