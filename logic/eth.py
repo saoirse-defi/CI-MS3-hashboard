3581fb85206350d504368bd238df62cdff2b9366
saoirse-defi <saoirsef@yahoo.co.uk>
2021-08-26 09:32:22 +0000

    change transaction cunt to 1000 now that transactions are working again

--- logic/eth.py
+++ logic/eth.py
@@ -53,7 +53,7 @@ def get_transactions(address):
         complete_transaction_list = list_eth + list_erc + list_nft
 
         for transaction in complete_transaction_list:
-            if count < 10:
+            if count < 1000:
                 data = {
                     'time': time.strftime(
                         "%d-%m-%Y", time.localtime(
