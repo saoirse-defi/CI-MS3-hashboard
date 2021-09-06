42fd242463111731f64cfa699e0ae20f06fc101a
saoirse-defi <saoirsef@yahoo.co.uk>
2021-09-06 10:25:58 +0000

    fix loading spinner on page load bug

--- static/search.js
+++ static/search.js
@@ -1,11 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var search_button = document.getElementById('search-submit');
     var loading = document.getElementsByClassName('loading');
+  });
 
-    loading.style.display = 'none';
-
-    search_button.addEventListener('click', () => {
-      loading.style.display = 'block';
-    });
-
+  search_button.addEventListener('click', () => {
+    loading.style.zIndex = 99999;
   });
