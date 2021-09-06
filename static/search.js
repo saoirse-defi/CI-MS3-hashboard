a6c7e70a5919bd81528087ea96791462f93fa0cd
saoirse-defi <saoirsef@yahoo.co.uk>
2021-09-06 09:49:11 +0000

    add spinner logic, already logged in button to navigate back to the hashboard

---
+++ static/search.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var search_button = document.getElementById('search-submit');
+    var loading = document.getElementsByClassName('loading');
+
+  });
+
+search_button.addEventListener('click', () => {
+    loading.style.display = 'block';
+  });
