d02b57243b2456b50d5b7424b87437ab9a992df2
saoirse-defi <saoirsef@yahoo.co.uk>
2021-09-06 09:52:31 +0000

    fix spinner on page load

--- static/search.js
+++ static/search.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var search_button = document.getElementById('search-submit');
     var loading = document.getElementsByClassName('loading');
 
+    loading.style.display = 'none';
+
   });
 
 search_button.addEventListener('click', () => {
